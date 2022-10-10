00aae50bfa749d9e86d247dd1d8938e05995904d
koushal AR <95262944+Koushal-Anitha-Raja@users.noreply.github.com>
2022-10-10 00:38:23 -0400

    Add files via upload

---
+++ dynamicprogramming3/minimumfallingpath.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minFallingPathSum(self, matrix: List[List[int]]) -> int:
+        #iterating the matrix using loop and considering the the iteration from second row
+        
+        n=len(matrix)
+        for i in range (1,n):
+            for j in range(n):
+                #two checks in place
+                if j==0:
+                    matrix[i][j]+=min(matrix[i-1][j],matrix[i-1][j+1])
+                #again two check if it is the last column 
+                elif  j==n-1:
+                    matrix[i][j]+=min(matrix[i-1][j],matrix[i-1][j-1])
+                    #three checks for the middle element
+                else:
+                    matrix[i][j]+=min(matrix[i-1][j],matrix[i-1][j-1],matrix[i-1][j+1])
+             #returning the minimum element of last row
+            
+        return min(matrix[-1])
+                    
+                    
